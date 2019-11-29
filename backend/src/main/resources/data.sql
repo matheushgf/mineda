c006e34fb371e77e9ab56d69d18a5812d3e8c070
Mayara Cristina da Silva Ferreira <mcristina@img.com.br>
2019-11-29 19:39:33 -0300

    Insert autorização, anotação

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,5 +12,15 @@ INSERT INTO produto (nome, descricao, preco, tipo, validade, pedido) VALUES ('Re
 INSERT INTO produto (nome, descricao, preco, tipo, validade, pedido) VALUES ('X salada', 'completo', 13.50, 'alimento', {ts '2019-10-01 18:47:52.69'}, 2);
 
 INSERT INTO USR_USUARIO(USR_NOME, USR_SENHA) VALUES
+('admin','admin'),
 ('Matheus', '1234'),
 ('Mayara', '1234');
+
+INSERT INTO AUT_AUTORIZACAO(AUT_NOME) VALUES('ROLE_USUARIO');
+INSERT INTO AUT_AUTORIZACAO(AUT_NOME) VALUES('ROLE_ADMIN');
+
+INSERT INTO UAU_USUARIO_AUTORIZACAO(USR_ID, AUT_ID)
+SELECT USR_ID, AUT_ID
+FROM USR_USUARIO, AUT_AUTORIZACAO
+WHERE USR_NOME = 'admin'
+AND AUT_NOME = 'ROLE_ADMIN';
